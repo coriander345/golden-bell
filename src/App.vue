8df5341e6a737de2c4a2efc6d27f819e053ba109
wonjae jun <woncha7764@gmail.com>
2022-10-07 01:06:04 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods:{
+    setName(){
+      this.name = 'johnjohn'
+    }  
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button :type="button" @click="setName">{{name.length? name : 'no name'}}</button>
+  </div>
+</template>
+
